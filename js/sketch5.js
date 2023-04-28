cf79a382d2ded22b07ce7e7e18a2274add15d289
Sean Shmulevich <seanshmulevich@seans-mbp-3.lan>
2023-04-27 20:41:05 -0400

    fixed

--- js/sketch5.js
+++ js/sketch5.js
@@ -1,44 +1,48 @@
-let symbolSize = 10;
-let streams = [];
-let capture;
-let videoImage;
+//by Emily Xie
+var streams = [];
+var fadeInterval = 1.4;
+var symbolSize = 15;
 
 function setup() {
     createCanvas(800, 600);
-    pixelDensity(1);
     background(0);
 
-    videoImage = createImage(width, height);
-    let x = 0;
-    for (let i = 0; i <= width / symbolSize; i++) {
-        let stream = new Stream();
-        stream.generateSymbols(x, random(-1000, 0));
+    // set up video capture
+    capture = createCapture(VIDEO);
+    capture.size(width, height);
+    capture.hide();
+
+    var x = 0;
+    for (var i = 0; i <= width / symbolSize; i++) {
+        var stream = new Stream();
+        stream.generateSymbols(x, random(-2000, 0));
         streams.push(stream);
-        x += symbolSize;
+        x += symbolSize
     }
 
+    textFont('Consolas');
     textSize(symbolSize);
 }
 
 function draw() {
+    // display video capture as the background
+    image(capture, 0, 0, width, height);
 
-    // Draw the current frame of the video feed to the image object
-    videoImage.copy(capture, 0, 0, capture.width, capture.height, 0, 0, width, height);
-
-    // Set the background to the video image
-    background(videoImage);
     streams.forEach(function (stream) {
         stream.render();
     });
 }
 
+
 function Symbol(x, y, speed, first, opacity) {
     this.x = x;
     this.y = y;
     this.value;
+
     this.speed = speed;
     this.first = first;
     this.opacity = opacity;
+
     this.switchInterval = round(random(2, 25));
 
     this.setToRandomSymbol = function () {
@@ -59,21 +63,28 @@ function Symbol(x, y, speed, first, opacity) {
     this.rain = function () {
         this.y = (this.y >= height) ? 0 : this.y += this.speed;
     }
+
 }
 
 function Stream() {
     this.symbols = [];
-    this.totalSymbols = round(random(5, 30));
+    this.totalSymbols = round(random(5, 35));
     this.speed = random(5, 22);
 
     this.generateSymbols = function (x, y) {
         var opacity = 255;
         var first = round(random(0, 4)) == 1;
         for (var i = 0; i <= this.totalSymbols; i++) {
-            symbol = new Symbol(x, y, this.speed, first, opacity);
+            symbol = new Symbol(
+                x,
+                y,
+                this.speed,
+                first,
+                opacity
+            );
             symbol.setToRandomSymbol();
             this.symbols.push(symbol);
-            opacity -= (255 / this.totalSymbols) / 1.5;
+            opacity -= (255 / this.totalSymbols) / fadeInterval;
             y -= symbolSize;
             first = false;
         }
